4a2e22c5d7ecfbee51c5ca0f953e3adff523f528
Tanya Zvereva <2022>
2019-09-23 11:28:20 +0300

    update admin

---
+++ polls/views.py
@@ -0,0 +1,44 @@
+from django.core import serializers
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect, JsonResponse
+from django.template import loader
+from django.urls import reverse
+
+from polls.models import Question, Choice
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # question_list = serializers.serialize('json', latest_question_list)
+    # return HttpResponse(question_list, content_type='application/json')
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    # question_list = serializers.serialize('json', question)
+    # return HttpResponse(question_list, content_type='application/json')
+    return render(request, 'polls/results.html', {'question': question})
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
